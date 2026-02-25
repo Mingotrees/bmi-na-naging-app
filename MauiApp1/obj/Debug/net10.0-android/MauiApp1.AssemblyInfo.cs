cd73f5767df54c64cc566e341ee5b91bb4807ac0
Mingotrees <jhanell.mingo@gmail.com>
2026-02-26 05:32:23 +0800

    ignore

--- MauiApp1/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+abd5687f6c5e6d93e9f8054391ca0e40f60746a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
